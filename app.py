d0cf4041dbd04e69438027776adff323bab1b85c
danulawa <danula.wa@gmail.com>
2023-12-10 11:42:47 +0530

    ui Test

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ import extract_usd
 import extract_aud
 import extract_eur
 
+# Set to store unique IP addresses
+unique_visitors = set()
+
 app = Flask(__name__)
 
 @app.route('/')
 def back_home():
-    return render_template('index.html')
+
+    # Get the visitor's IP address
+    ip_address = request.remote_addr
+
+    # Check if the IP address is not in the set
+    if ip_address not in unique_visitors:
+        unique_visitors.add(ip_address)
+
+    # Retrieve total visitor count
+    total_visitors = len(unique_visitors)
+
+    return render_template('index.html', total_visitors=total_visitors)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -31,8 +45,7 @@ def home():
         if not currency:
             currency = 'LKR'
 
-        ##Formatting assigned values
-        ################################################################
+        # Formatting assigned values
 
         basic_salary = int(basic_salary)
         allowances = int(allowances)
@@ -40,15 +53,13 @@ def home():
         currency = str(currency)
 
 
-        ##Calculating & Formatting annual_salary
-        ################################################################
+        # Calculating & Formatting annual_salary
 
         annual_salary = (basic_salary + allowances) * 12
         annual_salary = int(annual_salary)
 
 
-        ##Converting USD to LKR
-        ################################################################
+        # Converting USD to LKR
 
         if currency == 'USD':
 
@@ -61,8 +72,7 @@ def home():
             converted_deductions = deductions*usd_rate
 
 
-        ##Converting EUR to LKR
-        ################################################################
+        # Converting EUR to LKR
 
         if currency == 'EUR':
 
@@ -75,8 +85,7 @@ def home():
             converted_deductions = deductions*eur_rate
 
 
-        ##Converting GBP to LKR
-        ################################################################
+        # Converting GBP to LKR
 
         if currency == 'GBP':
 
@@ -89,8 +98,7 @@ def home():
             converted_deductions = deductions*gbp_rate
 
 
-        ##Converting AUD to LKR
-        ################################################################
+        # Converting AUD to LKR
 
         if currency == 'AUD':
 
@@ -103,8 +111,7 @@ def home():
             converted_deductions = deductions*aud_rate
 
 
-        ##Converting AUD to LKR
-        ################################################################
+        # Converting AUD to LKR
 
         if currency == 'LKR':
 
@@ -116,8 +123,7 @@ def home():
             converted_deductions = deductions*lkr_rate
 
 
-        ##Formatting converted values
-        ################################################################
+        # Formatting converted values
 
         converted_annual = int(converted_annual)
         converted_basic = int(converted_basic)
@@ -125,8 +131,7 @@ def home():
         converted_deductions = int(converted_deductions)
 
 
-        ##Calculating Income Tax
-        ################################################################
+        # Calculating Income Tax
 
         if converted_annual in range(0, 1200001):
             income_tax = 0
@@ -146,35 +151,47 @@ def home():
             income_tax = (500000 / 12) * 0.06 + (500000 / 12) * 0.12 + (500000 / 12) * 0.18 + (500000 / 12) * 0.24 + (
                         500000 / 12) * 0.30 + ((converted_annual - 3700000) / 12) * 0.36
 
-        ##Calculating EPF/ETF Deductions
-        ################################################################
+        # Calculating EPF/ETF Deductions
 
-        epf_etf_amount = (converted_basic * 8 / 100)
-
-        ##Calculating Final Net Salary
-        ################################################################
-
-        net_salary = (converted_basic + converted_allowances - income_tax - epf_etf_amount) - converted_deductions
+        epf_etf_amount = (converted_basic * 20 / 100) + (converted_allowances * 3 / 100)
+        epf_8_amount = (converted_basic * 8 / 100)
 
 
-        ##Formatting final outputs and return values
-        ################################################################
+        # Calculating Final Net Salary
+
+        net_salary = (converted_basic + converted_allowances - income_tax - epf_8_amount) - converted_deductions
+
+
+        # Formatting final outputs and return values
 
         net_salary = round(net_salary,2)
         income_tax = round(income_tax,2)
         epf_etf_amount = round(epf_etf_amount,2)
+        epf_8_amount = round(epf_8_amount,2)
 
         net_salary = str(net_salary)
         income_tax = str(income_tax)
         epf_etf_amount = str(epf_etf_amount)
+        epf_8_amount = str(epf_8_amount)
 
         net_salary = 'LKR ' + net_salary
         income_tax = 'LKR ' + income_tax
         epf_etf_amount = 'LKR ' + epf_etf_amount
+        epf_8_amount = 'LKR ' + epf_8_amount
 
-        return render_template('output.html', net_salary=net_salary, income_tax=income_tax, epf_etf_amount=epf_etf_amount)
+        # Get the visitor's IP address
+        ip_address = request.remote_addr
 
-    return render_template('index.html')
+        # Check if the IP address is not in the set
+        if ip_address not in unique_visitors:
+            unique_visitors.add(ip_address)
+
+        # Retrieve total visitor count
+        total_visitors = len(unique_visitors)
+
+        return render_template('output.html', net_salary=net_salary, income_tax=income_tax, epf_8_amount=epf_8_amount, epf_etf_amount=epf_etf_amount, total_visitors=total_visitors)
+
+    return render_template('index.html', total_visitors=total_visitors)
 
 
 if __name__ == '__main__':
